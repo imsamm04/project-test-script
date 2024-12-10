76046159a2b98dba549edba995e830bd9dbbae9b
viet.dinhduc <46956028+imsamm04@users.noreply.github.com>
2024-12-10 22:55:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-// ランダムな文字列を生成する / Generate random string
+function showDate() {
+    const now = new Date().toISOString();
+    return now;
+}
+
 function uuidV4() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
 
-// ランダムな整数を生成する / Generate random integer
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-// ランダムな配列を生成する / Generate random array
 function getRandomArray(max, length) {
     let array = [];
     for (let i = 0; i < length; i++) {
@@ -19,7 +21,6 @@ function getRandomArray(max, length) {
     return array
 }
 
-// ランダムな Json 文字列を生成する / Generate random json string
 function getRandomJson(max, length) {
     let hash = {};
     for (let i = 0; i < length; i++) {
@@ -28,7 +29,6 @@ function getRandomJson(max, length) {
     return JSON.stringify(hash)
 }
 
-// ランダムな日時を生成する / Generate random datetime
 function getRandomDateTime(fromDate) {
     let d1 = new Date(fromDate);
     const d2 = Date.now();
@@ -45,24 +45,18 @@ function getRandomDateTime(fromDate) {
     return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
 }
 
-// ページ遷移時に line_id を 自動でクエリパラメータに付与する / Add line_id to query parameter when moving to another page.
-function addLineIdToQueryParam() {
+function getLineIdParam() {
     const params = new Proxy(new URLSearchParams(window.location.search), { get: (searchParams, prop) => searchParams.get(prop) });
-    const line_id = params.line_id;
+    return params.line_id
+}
 
-    if (line_id === null) { // add
-        return;
+
+function addLineIdToQueryParam() {
+    const line_id = getLineIdParam();
+    if (line_id) {
+        let element = document.getElementById("require-line-id");
+        let old_href = element.getAttribute("href");
+        let new_href = old_href + `?` + new URLSearchParams({ line_id: line_id }).toString();
+        element.setAttribute("href", new_href);
     }
-
-    const query_parameter = 'line_id=' + line_id;
-
-    const element = document.getElementById('add-line-id');
-    let target_href = element.getAttribute('href');
-
-    if (target_href.indexOf('?') !== -1) {
-        target_href += '&' + query_parameter;
-    } else {
-        target_href += '?' + query_parameter;
-    }
-    element.setAttribute('href', target_href);
 }
